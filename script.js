3bb36c38d605ac6b054140bfa25dfbaea2a5d625
zechariahfb04 <zechariahfb@gmail.com>
2026-02-05 00:22:42 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function checkAnswer() {
+  const input = document.getElementById("answer").value.toLowerCase();
+  const message = document.getElementById("message");
+
+  if (input === "yournicknamehere") {
+    window.location.href = "room2.html";
+  } else {
+    message.textContent = "❌ Try again, love.";
+  }
+}
