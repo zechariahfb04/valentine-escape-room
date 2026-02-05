375ede229971e55f1f663e88f2696d33c4afceac
zechariahfb04 <zechariahfb@gmail.com>
2026-02-05 00:35:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function checkAnswer() {
   const input = document.getElementById("answer").value.toLowerCase();
   const message = document.getElementById("message");
 
-  if (input === "min Solstrale") {
+  if (input === "min solstrale") {
     window.location.href = "room2.html";
   } else {
     message.textContent = "❌ Try again, Sunshine☀️";
